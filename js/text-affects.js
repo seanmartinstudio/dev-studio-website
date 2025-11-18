bdbaf2c05555c110caffd06e512311026d2965e9
Sean Martin <sean@seanmartinstudio.com>
2025-11-18 10:12:28 -0800

    feat: adds text affects

---
+++ js/text-affects.js
@@ -0,0 +1,49 @@
+// scroll-fade.js
+
+// Register the plugin
+gsap.registerPlugin(ScrollTrigger, ScrambleTextPlugin);
+
+// Scramble <h1> on page load
+gsap.to("h1", {duration: 1, scrambleText: "Eternal September"});//or customize things:
+gsap.to("h1", {
+  duration: 2, 
+  scrambleText: {
+    text: "Eternal September", 
+    chars: "XO", 
+    speed: 1, 
+    newClass: "myClass"
+  }
+});
+
+// Scroll trigger fade in <artcile>
+gsap.utils.toArray("article").forEach((section) => {
+    gsap.from(section, {
+        opacity: 0,
+        y: 30,
+        duration: 2,
+        ease: "power2.out",
+
+        scrollTrigger: {
+            trigger: section,
+            start: "top center",
+            toggleActions: "play none none reverse"
+        }
+    });
+});
+
+// Scroll trigger fade in <section>
+gsap.utils.toArray("section").forEach((section) => {
+    gsap.from(section, {
+        opacity: 0,
+        y: 30,
+        duration: 2,
+        ease: "power2.out",
+
+        scrollTrigger: {
+            trigger: section,
+            start: "top 85%",
+            toggleActions: "play none none reverse"
+        }
+    });
+});
+
